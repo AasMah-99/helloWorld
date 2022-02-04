334bb71afda04c72df07b22ab46f56b87d5b5cda
AasMah-99 <aasafmahiuddin@gmail.com>
2022-02-04 09:38:41 +0530

    first bulk push to git

---
+++ helloworlddemo/src/helloworlddemo/Helloworlddemo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package helloworlddemo;
+
+/**
+ * @author mohamed
+ *
+ */
+public class Helloworlddemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
